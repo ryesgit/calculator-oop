2283aec772f2d439825ede61211457159bce5948
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-05-21 01:26:36 +0800

    Initial commit

---
+++ backend/Calculator.py
@@ -0,0 +1,22 @@
+'''
+
+filename: Calculator.py
+
+This module contains the Calculator class.
+Pass these as parameters to the constructor:
+- First number
+- Second number
+- Operation register
+- Result register
+- Calculator name
+
+'''
+
+# Pseudocode
+'''
+
+1. Create constructor (Numbers to calculate, operation register, result register, calculator name)
+2. Create methods (solve, clear, get result, get name)
+3. Look out for errors!
+
+'''
