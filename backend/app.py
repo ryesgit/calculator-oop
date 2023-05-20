6d49f3356a0f0b721316b6a2f5d2a6e80f240f4d
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-05-21 06:57:17 +0800

    Allow creation of calculator instances

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+from Calculator import Calculator
 
 app = Flask(__name__)
 
@@ -8,3 +10,13 @@ CORS(app)
 @app.route('/')
 def hello():
     return "Hello world."
+
+calculators = {}
+
+@app.route('/calculator/create', methods=['POST'])
+def create_calculator():
+    req_body = request.json
+    name = req_body.get('name')
+    calculator = Calculator(name)
+    # Add calculator to calculators dictionary
+    calculators[name] = calculator
