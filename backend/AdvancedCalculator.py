625efbc999aaaf37c726da6adfd650ae60e5d863
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-06-03 18:10:45 +0800

    Define AdvancedCalculator class methods

---
+++ backend/AdvancedCalculator.py
@@ -0,0 +1,10 @@
+'''
+Inherits from Calculator class.
+
+Aside from the basic add, subtract, divide, and multiply mechanism,
+this advanced calculator can also do other arithmetic functions, such as:
+values of trigonometric functions (sine, cosine, tangent),
+squaring numbers, returning the square root of numbers
+and lastly, performing logarithmic functions
+
+'''
